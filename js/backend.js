7da2626b497695a9cb579e0c8520dff87c1c5ee0
hatem.zidi <hatem.zidi@gmail.com>
2013-11-05 14:24:32 +0100

    added a backend counter page.

---
+++ js/backend.js
@@ -0,0 +1,26 @@
+/**
+ * Created with PhpStorm.
+ * User: h.zidi
+ * Date: 04/11/13
+ * Time: 16:43
+ *
+ */
+$(document).ready(function () {
+
+  function getCounter() {
+    var counter = $.ajax({
+      type: "GET",
+      url: "read.php",
+      async: false
+    }).success(function () {
+        setTimeout(function () {
+          getCounter();
+        }, 2500);
+      }).responseText;
+
+    $("#counter").html(counter + " dislikes, already!");
+  }
+
+  getCounter(); // start me
+
+});

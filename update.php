2c89084b46f3055c683b6faf4dfc135058a38e90
hatem.zidi <hatem.zidi@gmail.com>
2013-11-10 19:32:00 +0100

    update.php returns a blank image. added a welcom after install page for chrome extension

--- update.php
+++ update.php
@@ -8,6 +8,7 @@
  */
 header('Access-Control: allow *');
 header('Access-Control-Allow-Origin: *');
+header("Content-Type: image/gif");
 
 // config
 $dbfolder = $_SERVER["DOCUMENT_ROOT"] . "/data/";
@@ -27,4 +28,6 @@ $logdb->exec("UPDATE hits SET counter=counter+1 WHERE id=1");
 
 // close connection
 $logdb = null;
+
+echo file_get_contents(dirname(__FILE__).'/images/blank.gif');
 ?>

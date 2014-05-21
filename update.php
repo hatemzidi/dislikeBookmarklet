a4c85407305c032d507c3988294263565d0801e0
hatem.zidi <hatem.zidi@gmail.com>
2014-05-21 22:24:19 +0200

    - added tracker table based on ip, time and referrer - updating website page

--- update.php
+++ update.php
@@ -11,23 +11,39 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: image/gif");
 
 // config
-$dbfolder = $_SERVER["DOCUMENT_ROOT"] . "/data/";
+$dbfolder = dirname(__FILE__)  . "/data/";
 $dbname = "counter.sq3";
+
+// sniffer
+$browser = $_SERVER['HTTP_USER_AGENT']; // get the browser name
+$ip = $_SERVER['REMOTE_ADDR']; // get the IP address
+$referrer = $_SERVER['HTTP_REFERER']; //  page from which visitor came
+
+
+$logdb = new PDO("sqlite:" . $dbfolder . $dbname);
 
 // check if database file exists first
 if (!file_exists($dbfolder . $dbname)) {
-  $logdb = new PDO("sqlite:" . $dbfolder . $dbname);
   $logdb->exec("CREATE TABLE hits(id INTEGER PRIMARY KEY, counter INTEGER)");
+  $logdb->exec(
+    "CREATE TABLE tracker (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      browser TEXT default '',
+      ip varchar(15) NOT NULL default '',
+      d DATETIME NOT NULL default CURRENT_TIMESTAMP,
+      referrer TEXT NOT NULL default ''
+      )"
+  );
   $logdb->exec("INSERT INTO hits(id, counter) VALUES (1, 0)");
-} else {
-  $logdb = new PDO("sqlite:" . $dbfolder . $dbname);
 }
 
 // and boom ...
 $logdb->exec("UPDATE hits SET counter=counter+1 WHERE id=1");
+$logdb->exec("INSERT INTO tracker(browser, ip, referrer) VALUES ('$browser','$ip', '$referrer')");
+
 
 // close connection
 $logdb = null;
 
-echo file_get_contents(dirname(__FILE__).'/images/blank.gif');
+echo file_get_contents(dirname(__FILE__) . '/images/blank.gif');
 ?>
